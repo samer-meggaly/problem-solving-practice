f6650adee596f546c8c2b46f3cbe7b0ce2aa3984
Samer Makary <samer.samy.nazzir@gmail.com>
2015-12-28 01:41:15 +0200

    Cleaning LeetPrinter code and removing INCORRECT build tree function.

--- LeetCode/src/leetcode/utils/LeetPrinter.java
+++ LeetCode/src/leetcode/utils/LeetPrinter.java
@@ -2,14 +2,14 @@ package leetcode.utils;
 
 import java.util.Arrays;
 
-import leetcode.problems.tree.TreeNode;
-
 public class LeetPrinter {
 
 	public static void assertPrint(Object expected, Object actual,
 			String messagePrefix) {
-		if (!expected.equals(actual)) {
-			System.err.println(messagePrefix + actual.toString());
+		if (expected == null && actual == null)
+			return;
+		if (expected == null || !expected.equals(actual)) {
+			System.err.println(messagePrefix + actual);
 			System.err.flush();
 		}
 	}
@@ -22,44 +22,11 @@ public class LeetPrinter {
 		}
 	}
 
-	public static TreeNode toTree(Integer[] nums) {
-		return constructTree(nums, 1);
-	}
-
-	private static TreeNode constructTree(Integer[] nums, int i) {
-		if (i > nums.length || nums[i - 1] == null)
-			return null;
-		TreeNode node = new TreeNode(nums[i - 1]);
-		node.left = constructTree(nums, 2 * i);
-		node.right = constructTree(nums, 2 * i + 1);
-		return node;
-	}
-
 	public static String str(int[] array) {
-		StringBuilder sb = new StringBuilder();
-		sb.append('[');
-		for (int i = 0; i < array.length; i++) {
-			sb.append(array[i]);
-			sb.append((i == array.length - 1) ? "" : ", ");
-		}
-		sb.append(']');
-		return sb.toString();
+		return Arrays.toString(array);
 	}
 
 	public static String str(char[] array) {
-		StringBuilder sb = new StringBuilder();
-		sb.append('[');
-		for (int i = 0; i < array.length; i++) {
-			sb.append(array[i]);
-			sb.append((i == array.length - 1) ? "" : ", ");
-		}
-		sb.append(']');
-		return sb.toString();
-	}
-
-	public static void main(String[] args) {
-		TreeNode tree = toTree(
-				new Integer[] { 1, 2, 3, 4, 5, null, 7, null, null, 10, 11 });
-		System.out.println(tree);
+		return Arrays.toString(array);
 	}
 }
